d18ad0835052af3df64aed2d1fe11a9e16df98eb
hackhub817 <piyushguptaji123@gmail.com>
2022-08-20 09:52:04 +0530

    adding script

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const app = express();
-const port = 80;
-
+const port=process.env.PORT || 8000;
 // EXPRESS SPECIFIC STUFF
 app.use('/static', express.static('static')) // For serving static files
 app.use(express.urlencoded())
